79dcf4f14504985c850ad49d1c8df701c7d6502b
ilya-bychyk <ilya.bychyk@mail.com>
2022-03-21 22:36:55 +0500

    2 commit

--- src/test/java/tests/HealthHomeWork.java
+++ src/test/java/tests/HealthHomeWork.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class HealthHomeWork {
-    WebDriver driver;
+
+        WebDriver driver;
+
     @Test
     public void verifyWeight1Test() {
         driver = new ChromeDriver();
@@ -31,7 +34,6 @@ public class HealthHomeWork {
     }
     @Test
     public void verifyWeight3Test() {
-
         driver = new ChromeDriver();
         driver.get("https://healthunify.com/bmicalculator/");
         driver.findElement(By.xpath("//input[@name='wg']")).sendKeys("100");
@@ -42,20 +44,17 @@ public class HealthHomeWork {
     }
     @Test
     public void verifyWeight4Test() {
-
         driver = new ChromeDriver();
         driver.get("https://healthunify.com/bmicalculator/");
         driver.findElement(By.xpath("//input[@name='wg']")).sendKeys("80");
         driver.findElement(By.xpath("//input[@name='ht']")).sendKeys("180");
         driver.findElement(By.xpath("//input[@value='Calculate']")).click();
-
         String text = driver.findElement(By.xpath("//input[@name='desc']")).getAttribute("value");
         Assert.assertEquals(text, "Your category is Normal");
     }
 
     @Test
     public void verifyWeight5Test() {
-
         driver = new ChromeDriver();
         driver.get("https://healthunify.com/bmicalculator/");
         driver.findElement(By.xpath("//input[@name='wg']")).sendKeys("82");
